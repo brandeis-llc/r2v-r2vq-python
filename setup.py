fbe809eae9887b7992135c1a05a85576663d5b03
Keigh Rim <keigh.rim@gmail.com>
2021-08-11 12:10:32 -0400

    codebase separated, added basic setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('requirements.txt') as requirements:
+    requires = requirements.readlines()
+
+setuptools.setup(
+    name="r2vq",
+    version='0.1.0',
+    author="Brandeis-LLC",
+    description="r2vq-python package provides common operations over R2VQ dataset.",
+    install_requires=requires,
+    python_requires='>=3.7',
+    packages=['r2vq'],
+    entry_points={},
+)
